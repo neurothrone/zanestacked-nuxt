ac718cb2e7bea890a592315dea2c97ba266c49b6
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-05-04 12:26:21 +0200

    add initial project structure with pages, layout, and navbar components

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects">Projects</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
---
+++ pages/projects/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// Use this version in click handlers
+// const projects = await $fetch<[{ name: string, tech: string }]>("/api/projects");
+// Use this version in setups like this script block, to be called only once.
+const { data: projects } = await useFetch<[{ name: string, tech: string }]>("/api/projects");
+</script>
+
+<template>
+  <NuxtLayout name="projects">
+    <div>
+      <ul>
+        <li v-for="project in projects" :key="project.name">
+          <div>{{ project.name }}</div>
+          <div>{{ project.tech }}</div>
+        </li>
+      </ul>
+    </div>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  display: flex;
+  padding: 0;
+}
+
+li {
+  padding: 20px;
+}
+</style>
